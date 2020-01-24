8274f30d3de7c1956a9f078bdecba67c6971bb6e
Neha Patel <neha.r.p@gmail.com>
2020-01-23 21:13:41 -0800

    created util.py file

--- adv.py
+++ adv.py
@@ -2,6 +2,8 @@ from room import Room
 from player import Player
 from world import World
 
+from util import Stack, Queue
+
 import random
 from ast import literal_eval
 
@@ -10,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +31,7 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+print("room_graph", room_graph)
 
 
 # TRAVERSAL TEST
